0679fad86e15575688fe40efff19db0cca405723
Saswat <saswatsubhajyotimallick@gmail.com>
2020-09-11 10:28:32 +0530

    added utils

---
+++ utils/nms.py
@@ -0,0 +1,43 @@
+# -*- encoding=utf-8
+import numpy as np
+
+def single_class_non_max_suppression(bboxes, confidences, conf_thresh=0.2, iou_thresh=0.5, keep_top_k=-1):
+    if len(bboxes) == 0: return []
+
+    conf_keep_idx = np.where(confidences > conf_thresh)[0]
+
+    bboxes = bboxes[conf_keep_idx]
+    confidences = confidences[conf_keep_idx]
+
+    pick = []
+    xmin = bboxes[:, 0]
+    ymin = bboxes[:, 1]
+    xmax = bboxes[:, 2]
+    ymax = bboxes[:, 3]
+
+    area = (xmax - xmin + 1e-3) * (ymax - ymin + 1e-3)
+    idxs = np.argsort(confidences)
+
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # keep top k
+        if keep_top_k != -1:
+            if len(pick) >= keep_top_k:
+                break
+
+        overlap_xmin = np.maximum(xmin[i], xmin[idxs[:last]])
+        overlap_ymin = np.maximum(ymin[i], ymin[idxs[:last]])
+        overlap_xmax = np.minimum(xmax[i], xmax[idxs[:last]])
+        overlap_ymax = np.minimum(ymax[i], ymax[idxs[:last]])
+        overlap_w = np.maximum(0, overlap_xmax - overlap_xmin)
+        overlap_h = np.maximum(0, overlap_ymax - overlap_ymin)
+        overlap_area = overlap_w * overlap_h
+        overlap_ratio = overlap_area / (area[idxs[:last]] + area[i] - overlap_area)
+
+        need_to_be_deleted_idx = np.concatenate(([last], np.where(overlap_ratio > iou_thresh)[0]))
+        idxs = np.delete(idxs, need_to_be_deleted_idx)
+
+    return conf_keep_idx[pick]
